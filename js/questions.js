1ea0293bdd97de223daef827715b31f96b45dfff
emilavara <emilavara@hotmail.com>
2024-11-03 14:50:43 +0100

    finished game logic

--- js/questions.js
+++ js/questions.js
@@ -6,9 +6,7 @@ let scoreElement = document.getElementById('score');
 let questionElement = document.getElementById('question')
 let questionNumberElement = document.getElementById('question-number')
 let answerContainer = document.getElementById('answer-container')
-
-//get button
-let button = document.getElementById('test');
+let timeElement = document.getElementById('time')
 
 //fetch quiz and put response in questions variable
 await fetch('../data/quizData.json')
@@ -20,31 +18,6 @@ await fetch('../data/quizData.json')
         questions = data.html
     })
 
-
-function renderFunction() {
-    //clear answerContainer from content
-    answerContainer.innerHTML = ''
-    
-    //render answers
-    questionElement.textContent = questions[currentQuestion].question
-
-    for (let i in questions[currentQuestion].options) {
-        const p = document.createElement('p')
-
-        
-        p.textContent = questions[currentQuestion].options[i]
-       
-        p.addEventListener('click', function(e) {
-            answerQuestion(questions[currentQuestion].options[i]);
-        })
-    
-        answerContainer.appendChild(p)
-    }
-
-    //render score
-    scoreElement.textContent = 'score: ' +score;
-    questionNumberElement.textContent = 'question: ' + (currentQuestion + 1);
-}
 
 function answerQuestion(answer) {
     //check if chosen answer is correct
@@ -60,6 +33,12 @@ function answerQuestion(answer) {
     
         //rerender elements
         renderFunction();
+        
+        //reset timer
+        stopTimer();
+        
+        //restart new timer
+        questionTimer();
     } else {
         console.log('WRONG')
 
@@ -69,8 +48,75 @@ function answerQuestion(answer) {
         //dont increment score, as it was wrong
         //rerender elements
         renderFunction();
+
+        //reset timer
+        stopTimer();
+        
+        //restart new timer
+        questionTimer();
     }
     
 }
 
+//invoke interval variable outside of function to make it globally accessible 
+var timerInterval = undefined;
+
+function questionTimer() {
+    //set variables in seconds
+    time = 60;
+
+    //create a interval that decrements time every 1000ms
+    timerInterval = setInterval(() => {
+        //if time isn't 0 decrement
+        if (time != 0) {
+            time -= 1;
+            timeElement.textContent = '00:' + time;
+        }
+
+        //if time is 0, the timer has run out -- kill timer, declare time is out, go to next question
+        if (time === 0) {
+            clearInterval(timerInterval)
+            console.log('TIME IS OUT')
+            answerQuestion('time is out')
+        }
+    }, 1000)
+}
+
+function stopTimer() {
+    //reset timer text
+    timeElement.textContent = '01:00'
+
+    //clear timer interval
+    clearInterval(timerInterval)
+}
+
+
+
+function renderFunction() {
+    //clear answerContainer from content
+    answerContainer.innerHTML = ''
+    
+    //render answers
+    questionElement.textContent = questions[currentQuestion].question
+
+    for (let i in questions[currentQuestion].options) {
+        const p = document.createElement('p')
+
+        p.textContent = questions[currentQuestion].options[i]
+       
+        p.addEventListener('click', function(e) {
+            answerQuestion(questions[currentQuestion].options[i]);
+        })
+    
+        answerContainer.appendChild(p)
+    }
+
+    //render score
+    scoreElement.textContent = 'score: ' +score;
+    questionNumberElement.textContent = 'question: ' + (currentQuestion + 1);
+}
+
+
+//start game functions
 renderFunction();
+//questionTimer();
