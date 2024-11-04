1594ca52fde7de4336f2d683c5d270ad371e2b58
Fares-elloumi <elloumi.feres@gmail.com>
2024-11-04 21:55:32 +0100

    responsive

--- js/categories.js
+++ js/categories.js
@@ -5,20 +5,21 @@ const linkJs= document.getElementById("js");
 linkHtml.addEventListener("click", function(){
     let Kategori= this.id;
     console.log(Kategori);
-    localStorage.setItem("Kategori", Kategori)
+    localStorage.setItem("Kategori", Kategori);
+    window.location.assign("questions.html");
 });
 
 linkCss.addEventListener("click", function(){
     let Kategori= this.id;
     console.log(Kategori);
-    localStorage.setItem("Kategori", Kategori)
-    
+    localStorage.setItem("Kategori", Kategori);
+    window.location.assign("questions.html"); 
 });
 
 linkJs.addEventListener("click", function(){
     let Kategori= this.id;
     console.log(Kategori);
-    localStorage.setItem("Kategori", Kategori)
-
+    localStorage.setItem("Kategori", Kategori);
+    window.location.assign("questions.html");
 });
 
