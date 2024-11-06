bc7aa1e13039be1577c928e15489678c8b5d79f0
Ephraim Valladares <effelion@gmail.com>
2024-11-06 21:53:40 +0100

    fine tunning

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let ruleBtn = document.querySelector(".rules")
 let main = document.querySelector(".main")
 let rulesWindow = document.querySelector("#popContainer")
 let closePop = document.querySelector(".closePopUp")
+let soundBtn = document.querySelector(".speaker")
 
 // -------------------- localstorage-----------------------
 inputBtn.addEventListener("click", () => { 
@@ -13,10 +14,7 @@ inputBtn.addEventListener("click", () => {
     input.value= ""
 })
 
-// document.addEventListener("DOMContentLoaded", () => { 
-//     rulesWindow.classList.add("hidePopUp");
-// })
-
+// ------------------popup-------------------------------- 
 const togglePopUp = () => {
     rulesWindow.classList.toggle("showPopUp");
     main.classList.toggle("mainBlur");
@@ -25,9 +23,4 @@ const togglePopUp = () => {
 ruleBtn.addEventListener("click", togglePopUp)
 closePop.addEventListener("click", togglePopUp)
 
-// document.addEventListener("click", (event) => {
-//     if (!rulesWindow.contains(event.target) && event.target !== closePop) {
-//         rulesWindow.classList.remove("showPopUp");
-//         main.classList.remove("mainBlur");
-//     }
-// })
+
