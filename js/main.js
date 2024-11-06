a2bc809221e482659bf320aee296c5eb9a3c557e
Ephraim Valladares <effelion@gmail.com>
2024-11-06 20:40:18 +0100

    pop upp added

--- js/main.js
+++ js/main.js
@@ -3,9 +3,8 @@ let inputBtn = document.querySelector(".btnWrapper");
 let getData = localStorage.getItem("name");
 let ruleBtn = document.querySelector(".rules")
 let main = document.querySelector(".main")
-let rulesWindow = document.querySelector("#popUp")
-
-
+let rulesWindow = document.querySelector("#popContainer")
+let closePop = document.querySelector(".closePopUp")
 
 // -------------------- localstorage-----------------------
 inputBtn.addEventListener("click", () => { 
@@ -14,13 +13,21 @@ inputBtn.addEventListener("click", () => {
     input.value= ""
 })
 
-ruleBtn.addEventListener("click", () => { 
-    rulesWindow.classList.toggle("rulesPopUp");
-    main.classList.toggle("mainBlur");
-    rulesWindow.classList.remove("hidePopUp");
-    
-})
+// document.addEventListener("DOMContentLoaded", () => { 
+//     rulesWindow.classList.add("hidePopUp");
+// })
 
-document.addEventListener("DOMContentLoaded", () => { 
-    rulesWindow.classList.add("hidePopUp");
-})
+const togglePopUp = () => {
+    rulesWindow.classList.toggle("showPopUp");
+    main.classList.toggle("mainBlur");
+}
+
+ruleBtn.addEventListener("click", togglePopUp)
+closePop.addEventListener("click", togglePopUp)
+
+// document.addEventListener("click", (event) => {
+//     if (!rulesWindow.contains(event.target) && event.target !== closePop) {
+//         rulesWindow.classList.remove("showPopUp");
+//         main.classList.remove("mainBlur");
+//     }
+// })
