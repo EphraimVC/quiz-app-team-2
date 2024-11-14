080816b7deaaf854577f2c53a40f323cf9fec071
Emil <emil.avara@chasacademy.se>
2024-11-14 15:13:46 +0100

    started on mute function

--- js/headerComponent.js
+++ js/headerComponent.js
@@ -11,8 +11,8 @@ class nkHeader extends HTMLElement {
         <div  class="logo">
             <img id="header-logo-img" src="../img/logo.svg" alt="Logo with a smiling face wearing sunglasses">
         </div>
-        <div class="sound-icon">
-            <img  src="../img/lsicon_volume-filled.svg" alt="Sound icon">
+        <div id="mute-button">
+            <i id="volume-icon" class="bi bi-volume-up-fill"></i>
         </div>
     </header>`
     }
@@ -22,9 +22,22 @@ window.customElements.define('nk-header', nkHeader);
 
 document.addEventListener('DOMContentLoaded', () => {
     const logoElement = document.querySelector('#header-logo-img');
+    
+    const muteButton = document.getElementById('mute-button');
+    const audioElements = document.querySelectorAll('audio');
+
+    const volumeIcon = document.getElementById('volume-icon');
+
+    muteButton.addEventListener('click', () => {
+
+        //local storage för muted
+    })
 
     //if we are on index.html
     if (window.location.pathname === '/') {
         logoElement.style.visibility = 'hidden';
     }
+
+    
+
 })
